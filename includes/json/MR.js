1aed5bc04b1a6dc5fdfc86866a779d6cd38e7cdd
Kubuzeer <119004965+Kubuzer@users.noreply.github.com>
2024-09-29 14:56:48 +0200

    Delete includes/json/MR.js

--- includes/json/MR.js
+++
@@ -1,9 +0,0 @@
-var monthlies = {
-    "hilt": {
-        task: "Hilts Trader Monthly",
-        url: "#",
-        desc: "Buy monthly items such as a {leg_crest}",
-        img: "hilts",
-        boxcount: 1
-    },
-};
